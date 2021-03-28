4d5932558b377b8945dbed566da1585edcce39e1
ujos89 <48623147+ujos89@users.noreply.github.com>
2021-03-28 19:45:25 +0900

    Add files via upload

--- DL from Scratch/neural network/activationfunction.py
+++ DL from Scratch/neural network/activationfunction.py
@@ -1,8 +1,18 @@
 import numpy as np
 
+# activation function non-linear function
+
 def step_fucntion(x):
     return (x > 0).astype(np.int)
 
 def sigmoid(x):
     return 1 / (1+np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
+
+def identity_function(x):
+    return x
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x))
